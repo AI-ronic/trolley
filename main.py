761710bc7af42f5df6843dcf5adc54d8666db7dc
Tanwa Arpornthip <tanwa@tinfoil.lol>
2023-04-24 11:47:25 +0700

    working on parsing response

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 #import pandas as pd
 import requests
@@ -22,7 +23,7 @@ class ai21_model:
     self.api_key = api_key
     print(f'Using API endpoint: {self.api_endpoint}')
 
-  def get_model_response(self, prompt, num_results=1, max_tokens=64, temperature=0.7, top_k_return=0):
+  def get_raw_model_response(self, prompt, num_results=1, max_tokens=64, temperature=0.7, top_k_return=0):
     response = requests.post(
       self.api_endpoint,
       headers={"Authorization": f"Bearer {self.api_key}"},
@@ -34,7 +35,29 @@ class ai21_model:
         "topKReturn": top_k_return
       }
     )
+    return response['completions'][0]['data']['text']
+
+  @staticmethod
+  def parse_raw_response(raw_response):
+    interested_in = ['1', '2']
+    # Find all occurrences of 1 and 2 in raw_response
+    for answer in interested_in:
+    matches = re.findall("[12]", raw_response)
+
+    # Count the number of occurrences of 1 and 2
+    count_1 = matches.count("1")
+    count_2 = matches.count("2")
+
+    print(f"Number of 1's: {count_1}")
+    print(f"Number of 2's: {count_2}")
+
+
+
+  def get_model_response(self, prompt, num_results=1, max_tokens=64, temperature=0.7, top_k_return=0):
+    raw_response = self.get_raw_model_response(prompt, num_results, max_tokens, temperature, top_k_return)
+    response = parse_raw_response(raw_response)
     return response.json()
+  
 
 
 # Helper functions
@@ -56,9 +79,13 @@ def get_api_endpoint(model_type="j2-jumbo-instruct"):
       print('The value of the environment variable api_endpoint is:', api_endpoint)
   return api_endpoint
 
-if __name__ == "__main__":
+def run():
   api_key = get_api_key()
   api_endpoint = get_api_endpoint()
   model = ai21_model(api_key, api_endpoint)
   print(f'api_key')
   print(f'api_endpoint')
+  return model
+
+if __name__ == "__main__":
+  run()
